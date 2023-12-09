6b0ad3fd8207ffdf09d73e89fb33107f7a2102f6
erik465 <ehalasz06@gmail.com>
2023-12-09 11:50:06 +0100

    .

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import CatalogFilters from "../../components/CatalogFilters/CatalogFilters";
 import CarList from "../../components/CarList/CarList";
 import { StyledContainer } from "./CatalogPage.styled";
+import { NavLink } from "react-router-dom";
 
 const CatalogPage = () => {
   return (
     <StyledContainer>
+      <NavLink to="/favorites">Favorites</NavLink>
       <CatalogFilters />
       <CarList />
     </StyledContainer>
--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { StyledContainer, StyledHeading } from "./FavouritesPage.styled";
 import FavouritesList from "../../components/FavouritesList/FavouritesList";
 import { Suspense } from "react";
+import { NavLink } from "react-router-dom";
 const FavouritesPage = () => {
   return (
-    <StyledContainer>
-      <StyledHeading>
-        Your <span>Favourites</span> Page
-      </StyledHeading>
-      <Suspense fallback={<p>Loading...</p>}>
-        <FavouritesList />
-      </Suspense>
-    </StyledContainer>
+    <Suspense fallback={<p>Loading...</p>}>
+      <StyledContainer>
+        <StyledHeading>
+          <NavLink to="/catalog">Back to catalog</NavLink>
+          Your <span>Favourites</span> Page
+        </StyledHeading>
+        <Suspense fallback={<p>Loading...</p>}>
+          <FavouritesList />
+        </Suspense>
+      </StyledContainer>
+    </Suspense>
   );
 };
 
